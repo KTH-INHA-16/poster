b7bc33085f29bd89c442ecf976f2d98bce6f3b49
KTH-INHA-16 <kim1120318@naver.com>
2020-11-03 23:05:24 +0900

    Initial Commit

---
+++ poster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poster
+//
+//  Created by 김태훈 on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
