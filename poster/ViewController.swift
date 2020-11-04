6c301b961850a21269e91166352f6c55c027a9a1
KTH-INHA-16 <kim1120318@naver.com>
2020-11-04 17:06:45 +0900

    button 과 label을 다루는 법에 관련하여서

--- poster/ViewController.swift
+++ poster/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    @IBOutlet weak var bountyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let randomNum: UInt32 = arc4random_uniform(1000000) + 5000
+        currentValue = Int(randomNum)
+        bountyLabel.text = "$ \(currentValue)"
     }
-
-
+    
+    @IBAction func showAlert(){
+        let message: String = "현상금은 \(currentValue) 입니다"
+        
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        
+        let randomNum: UInt32 = arc4random_uniform(1000000) + 5000
+        currentValue = Int(randomNum)
+        bountyLabel.text = "$ \(currentValue)"
+    }
+    
 }
 
